56a406bbfa7ac5e8f283cee2497aa0b81ac8513d
Kubilay Karpat <karpatkubilay@gmail.com>
2017-01-24 15:37:16 +0300

    deploy

---
+++ scripts/chartist-goal-line.js
@@ -0,0 +1,99 @@
+/**
+ * Chartist.js plugin to display a "target" or "goal" line across the chart.
+ * Only tested with bar charts. Works for horizontal and vertical bars.
+ *
+ * Copyright (c) 2015 Yorkshire Interactive (yorkshireinteractive.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+(function(window, document, Chartist) {
+  'use strict';
+
+  var defaultOptions = {
+    // The class name so you can style the text
+    className: 'ct-goal',
+    // The axis to draw the line. y == vertical bars, x == horizontal
+    axis: 'y',
+    // What value the goal line should be drawn at
+    value: null,
+    // The text that will be displayed next to goal line
+    label: '',
+  };
+
+  Chartist.plugins = Chartist.plugins || {};
+
+  Chartist.plugins.ctGoalLine = function(options) {
+    options = Chartist.extend({}, defaultOptions, options);
+    return function ctGoalLine (chart) {
+
+      chart.on('created', function(context) {
+	// Calculate position for goal line
+        var projectTarget = {
+          y: function (chartRect, bounds, value) {
+            var targetLineY = chartRect.y1 - (chartRect.height() / bounds.max * value);
+
+            return {
+              x1: chartRect.x1,
+              x2: chartRect.x2,
+              y1: targetLineY,
+              y2: targetLineY
+            }
+          },
+          x: function (chartRect, bounds, value) {
+            var targetLineX = chartRect.x1 + (chartRect.width() / bounds.max * value);
+
+            return {
+              x1: targetLineX,
+              x2: targetLineX,
+              y1: chartRect.y1,
+              y2: chartRect.y2
+            }
+          }
+        };
+	// Calculate position for goal text
+        var textTarget = {
+          y: function (chartRect, bounds, value) {
+            var targetLineY = chartRect.y1 - (chartRect.height() / bounds.max * value);
+
+            return {
+              x: chartRect.x1 + 10,
+              y: targetLineY - 10,
+            }
+          },
+          x: function (chartRect, bounds, value) {
+            var targetLineX = chartRect.x1 + (chartRect.width() / bounds.max * value);
+
+            return {
+              x: targetLineX + 10,
+              y: chartRect.y2 + 10,
+            }
+          }
+        };
+
+        var targetLine = projectTarget[options.axis](context.chartRect, context.bounds, options.value)
+        var targetText = textTarget[options.axis](context.chartRect, context.bounds, options.value)
+        context.svg.elem('line', targetLine, options.className + ' ct-goal-line');
+        context.svg.elem('text', targetText, options.className + ' ct-goal-text').text(options.label);
+
+      });
+    }
+  }
+
+}(window, document, Chartist));
